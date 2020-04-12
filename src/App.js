0505a7cfce41a10b900e688b690c85d7af9e2d77
Habeeb <habeebodunsi@yahoo.com>
2020-04-12 14:23:20 +0100

    Feature/GitHub actions (#3)
    * Change Frontend text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployed from github
+          Deployed from github testing CI/CD pipeline
         </p>
         <a
           className="App-link"
